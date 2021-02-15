a3a0ebfb3fb5627ff7ae016fef13bea1701f7c0e
Souni13 <74820618+Souni13@users.noreply.github.com>
2021-02-15 08:46:45 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,106 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+ 
+var particles = [];
+var plinkos = [];
+
+var divisions=[];
+var score=0;
+var particle;
+var gameState= play;
+
+var divisionHeight=300;
+var score =0;
+
+function setup() {
+
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
+
+    line(30, 20, 85, 75);
+
+    
+}
+ 
+
+
+function draw() {
+  background("black");
+  textSize(20)
+  text("Score : "+score,20,30);
+  text("X:"+mouseX+", Y:"+mouseY, mouseX, mouseY);
+  text("500",25,508);
+  text("500",103,508);
+  text("500",185,508);
+  text("500",265,508);
+  text("100",345,508);
+  text("100",425,508);
+  text("100",505,508);
+  text("200",580,508);
+  text("200",665,508);
+  text("200",745,508);
+  Engine.update(engine);
+
+  
+  fill("white");  
+  if(frameCount%60===0){
+    particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
+    score++;
+ }
+ 
+  
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+  
+ 
+  for (var j = 0; j < particles.length; j++) {
+   
+     particles[j].display();
+   }
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
+} 
+function mousePressed(){
+  if(gameState!== "end"){
+   particle= new Particle(mouseX,10,10);
+  }
+}
